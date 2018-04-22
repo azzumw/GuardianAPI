354547545f4dcd433550beb0527de1c94ba5b481
Azzum Waqar <azzumwaqar@gmail.com>
2018-04-22 17:01:30 +0100

    Final Clean Code

--- GuardianAPI/app/src/main/java/com/example/azzumwaqar/guardianapi/MainActivity.java
+++ GuardianAPI/app/src/main/java/com/example/azzumwaqar/guardianapi/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.azzumwaqar.guardianapi;
 import android.app.LoaderManager;
 import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.content.Intent;
 import android.content.Loader;
-import android.os.AsyncTask;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,22 +23,22 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<NewsFeed>>{
 
     private TextView emptyStateTextView;
-    //listview
+    private ProgressBar progressBar;
     private ListView newsFeedListView;
-    //adapter
     private NewsAdapter adapter;
 
     private static final int NEWSFEED_LOADER_ID =1;
 
     private static final String GUARDIAN_API_URL = "http://content.guardianapis.com/search?api-key=bd204a3a-e27d-4f11-93c3-3e392bb3214b";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         newsFeedListView = findViewById(R.id.list);
         emptyStateTextView = findViewById(R.id.emptyStateTextView);
+        progressBar = findViewById(R.id.progressBar);
 
         adapter = new NewsAdapter(this,new ArrayList<NewsFeed>());
 
@@ -40,11 +46,30 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         newsFeedListView.setEmptyView(emptyStateTextView);
 
-        getLoaderManager().initLoader(NEWSFEED_LOADER_ID,null,this);
+        newsFeedListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                NewsFeed currentNewsFeed = adapter.getItem(position);
 
-//        NewsFeedAsyncTask task = new NewsFeedAsyncTask();
-//        task.execute(GUARDIAN_API_URL);
+                Uri webpage = Uri.parse(currentNewsFeed.getUrl());
 
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW,webpage);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+
+        ConnectivityManager cm = ((ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE));
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        if(activeNetwork != null && activeNetwork.isConnectedOrConnecting()){
+            getLoaderManager().initLoader(NEWSFEED_LOADER_ID,null,this);
+        }else{
+            progressBar.setVisibility(View.GONE);
+
+            emptyStateTextView.setText(R.string.NoInternetInfo);
+        }
     }
 
     @Override
@@ -57,7 +82,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void onLoadFinished(Loader<List<NewsFeed>> loader, List<NewsFeed> data) {
         adapter.clear();
 
+        progressBar.setVisibility(View.GONE);
+
         emptyStateTextView.setText(R.string.NoNewsInfo);
+
         if(data!=null && !data.isEmpty()) {
             adapter.addAll(data);
         }
@@ -97,6 +125,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         @Override
         public List<NewsFeed> loadInBackground() {
             Log.e("MainActivity","LoadinBackground executing...");
+
             if(mUrl ==null){
                 return null;
             }
@@ -105,27 +134,4 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             return result;
         }
     }
-
-    /*private class NewsFeedAsyncTask extends AsyncTask<String,Void,List<NewsFeed>>{
-
-        @Override
-        protected List<NewsFeed> doInBackground(String... urls) {
-            if(urls.length<1 || urls[0]==null){
-                return null;
-            }
-
-            List<NewsFeed> newsFeedList = Utils.fetchNewsFeedData(urls[0]);
-            return  newsFeedList;
-        }
-
-        @Override
-        protected void onPostExecute(List<NewsFeed> newsFeeds) {
-            adapter.clear();
-            if(newsFeeds!=null && !newsFeeds.isEmpty()){
-                adapter.addAll(newsFeeds);
-            }
-        }
-    }*/
-
-
 }
--- GuardianAPI/app/src/main/java/com/example/azzumwaqar/guardianapi/NewsFeed.java
+++ GuardianAPI/app/src/main/java/com/example/azzumwaqar/guardianapi/NewsFeed.java
@@ -6,11 +6,19 @@ public class NewsFeed {
     private String title;
     private String section;
     private String date;
+    private String url;
 
     public NewsFeed(String title, String section, String date) {
         this.title = title;
         this.section = section;
         this.date = date;
+    }
+
+    public NewsFeed(String title, String section, String date, String url) {
+        this.title = title;
+        this.section = section;
+        this.date = date;
+        this.url = url;
     }
 
     public String getTitle() {
@@ -23,5 +31,9 @@ public class NewsFeed {
 
     public String getDate() {
         return date;
+    }
+
+    public String getUrl() {
+        return url;
     }
 }
